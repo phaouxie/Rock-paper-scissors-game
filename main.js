f047fd6a36b70550c4fcc3fddd2fec74c3fdbad6
phaouxie <fauziyya.hayatu@aun.edu.ng>
2022-08-28 17:00:15 +0100

    RPS

---
+++ main.js
@@ -0,0 +1,143 @@
+class GamePiece {
+  constructor(piece) {
+    this._piece = piece;
+  }
+
+  getPiece() {
+    return this._piece;
+  }
+
+  getHTML() {
+    return this._piece === "rock"
+      ? `<button class="btn-rotate rock" name="rock">
+        <span class="wrapper">
+        <img src="./images/icon-rock.svg" alt="rock icon">
+        </span>
+      </button>`
+      : this._piece === "paper"
+      ? `<button class="btn-rotate     paper" name="paper">
+        <span class="wrapper">
+          <img src="./images/icon-paper.svg" alt="paper icon">
+        </span>
+      </button>`
+      : this._piece === "scissors"
+      ? `<button class="btn-rotate scissors" name="scissors">
+      <span class="wrapper">
+        <img src="./images/icon-scissors.svg" alt="scissors icon">
+      </span>
+      </button>`
+      : null;
+  }
+  getRandomPiece() {
+    const choices = ["paper", "rock", "scissors"];
+    this._piece = choices[Math.floor(Math.random() * choices.length)];
+    return this._piece;
+  }
+}
+
+
+
+// show the selection || hide the main
+const main = document.getElementById('main');
+const selection = document.getElementById('selection');
+
+
+
+
+class Display {
+  constructor(score) {
+    this._score = 0;
+  }
+
+  displayTile(choice, player) {
+    const userPicked = document.querySelector("#you-picked");
+    const computerPicked = document.querySelector("#computer-picked");
+
+    player === "user"
+      ? (userPicked.innerHTML = choice.getHTML())
+      : (computerPicked.innerHTML = choice.getHTML());
+  }
+
+  displayTileWinner(winner) {
+    const win = document.querySelector("#win");
+    const scoreBoard = document.querySelector("#score");
+    console.log(winner);
+    if (winner === "user") {
+      win.innerHTML = "You win";
+      this._score += 1;
+      scoreBoard.innerHTML = `${this._score}`;
+    }
+    else if (winner === "Draw"){
+      win.innerHTML = "Tie!"
+    }
+    else {
+      win.innerHTML = "You Lose";
+    }
+  }
+
+  checkWinner(userChoice, computerChoice) {
+    if (
+    
+      (userChoice.getPiece() === "paper" &&
+        computerChoice.getPiece() === "rock") ||
+      (userChoice.getPiece() === "rock" &&
+        computerChoice.getPiece() === "scissors") ||
+      (userChoice.getPiece() === "scissors" &&
+        computerChoice.getPiece() === "paper")
+    ) {
+      console.log('working');
+      return "user";
+    
+    }
+    else if(userChoice.getPiece() === computerChoice.getPiece()){
+      return "Draw";
+
+  }    
+    else {
+      return "computer";
+    }
+
+    
+
+
+
+
+  }
+    
+}
+
+const buttons = document.querySelectorAll(".btn-rotate");
+const rockTile = document.querySelector(".rock");
+const display = new Display(0);
+
+
+
+
+let userChoice 
+let computersChoice = new GamePiece("none");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let piece = button.getAttribute("name");
+    userChoice = new GamePiece(piece);
+    computersChoice.getRandomPiece();
+    display.displayTile(userChoice, "user");
+    display.displayTile(computersChoice, "computer");
+    display.displayTileWinner(display.checkWinner(userChoice, computersChoice));
+      // let mainDiv = document.querySelector('#main');
+      
+      main.style.display = 'none';
+    selection.style.display = 'flex';
+    
+
+  });
+});
+
+
+const reset = document.querySelector('#reset');
+
+reset.addEventListener('click', ()=>{
+  main.style.display = 'flex';
+    selection.style.display = 'none';
+  
+})
